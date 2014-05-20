ace7e9270aa531c1728c0ea7a0c5a8a5f13f1f39
Geoffroy PLANQUART <geoffroy@W530.aethelflaed.com>
2014-05-20 11:23:45 +0200

    Added various test cases

--- test/subclass_test.rb
+++ test/subclass_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class SubclassTest < BaseTest
   def test_subclass_sequence
-    n = 20
+    n = 200
 
     n.times do
       SubclassSequenceModel.create
@@ -12,10 +12,35 @@ class SubclassTest < BaseTest
   end
 
   def test_subclass_sequence_increment_when_base_does
-    FirstSequencedModel.create
-    m1 = SubclassSequenceModel.create
+    n = 200
 
-    assert_equal m1.auto_increment, 2
+    n.times do |i|
+      FirstSequencedModel.create
+      m1 = SubclassSequenceModel.create
+
+      assert_equal i*2 + 2, m1.auto_increment
+    end
+  end
+
+  def test_mixed_sequence_uniqueness
+    n = 100
+    n.times do
+      FirstSequencedModel.create
+      SubclassSequenceModel.create
+    end
+
+    assert_equal FirstSequencedModel.only(:auto_increment).uniq.size, n * 2
+  end
+
+  def test_mixed_sequence_consistency
+    n = 100
+
+    n.times do
+      FirstSequencedModel.create
+      SubclassSequenceModel.create
+    end
+
+    assert_equal FirstSequencedModel.only(:auto_increment).map(&:auto_increment).sort, (1..n*2).to_a
   end
 end
 
